f7a89ba7abed1e47539fe4ae781ae24fe40c3a0d
Darren Petersen <darrenpetersen16@gmail.comgit config --global user.name Darren>
2022-02-01 20:04:11 +0200

    Final Commit

--- src/views/Projects/Projects.vue
+++ src/views/Projects/Projects.vue
@@ -2,16 +2,7 @@
   <h2 class="fw-bold mb-5">Project Page</h2>
   <div class="container">
     <div v-if="projects.length">
-      <div
-        v-for="project in projects"
-        :key="project.id"
-        :title="project.title"
-        :details="project.details"
-        :img="project.img"
-        :github="project.github"
-        :netlify="project.netlify"
-        class="project"
-      >
+      <div v-for="project in projects" :key="project.id" class="project">
         <router-link
           :to="{
             name: 'ProjectDetails',
